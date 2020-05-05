ed4550febfd6bf35f1b1d1e10e024e55370c6462
Gor <gorhakobyann@gmail.com>
2020-05-05 19:31:24 +0400

    background images

---
+++ src/resources/Images.js
@@ -0,0 +1,19 @@
+import image0 from './background_0.jpg'
+import image1 from './background_1.png'
+import image2 from './background_2.jpg'
+import image3 from './background_3.jpg'
+import image4 from './background_4.png'
+import image6 from './background_6.png'
+import image7 from './background_7.jpg'
+import image8 from './background_8.jpg'
+
+export default [
+    image0,
+    image1,
+    image2,
+    image3,
+    image4,
+    image6,
+    image7,
+    image8,
+]
