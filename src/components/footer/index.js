c50d40cf6aa2400432281b92e7ba0a4127e1c5cc
Gor <gorhakobyann@gmail.com>
2020-05-05 19:34:06 +0400

    footer component

---
+++ src/components/footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.css'
+
+export default function Footer(props) {
+    // if (props.history.location.pathname === ROUTES.SIGN_IN)
+
+    return (
+        <div></div>
+    )
+
+}
