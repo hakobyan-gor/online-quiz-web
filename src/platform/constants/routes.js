2bdf20d6dbcfebaab257fe332936708a71daf263
Gor <gorhakobyann@gmail.com>
2020-05-05 19:38:42 +0400

    routs

---
+++ src/platform/constants/routes.js
@@ -0,0 +1,13 @@
+const ROUTES = {
+    HOME        :   '/home',
+    SIGN_IN     :   '/sign-in',
+    SIGN_UP     :   '/sign-up',
+    PROFILE     :   '/profile',
+    CONTACT_US  :   '/contact-us',
+    ABOUT_US    :   '/about-us',
+    WELCOME     :   '/welcome',
+    QUIZZES     :   '/quizzes',
+    ERROR       :   '/error',
+}
+
+export default ROUTES
