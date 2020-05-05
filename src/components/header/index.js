e5463ddb294a8755ef2b5069234ead9982f1d3fa
Gor <gorhakobyann@gmail.com>
2020-05-05 19:34:26 +0400

    header component

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+function Header(props) {
+    // if(props.history.location.pathname === ROUTES.SIGN_IN)
+    return (
+        <div>
+            
+        </div>
+    )
+
+}
+
+export default withRouter(Header)
