31313eafccbc59f3f026b3be396ce4acaace8164
Gor Hakobyan <gorhakobiann@gmail.com>
2020-05-30 21:43:40 +0400

    sign up service

--- src/platform/services/sign-up/SignUpService.js
+++ src/platform/services/sign-up/SignUpService.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import API_URL from "../../constants/environment";
+import ROUTES from '../../constants/routes'
 
 class SignUpService {
 
     signUp(signUpRequset) {
-        return axios.post(API_URL + '/sign-up', signUpRequset)
+        return axios.post(API_URL + ROUTES.SIGN_UP, signUpRequset)
     }
 
 }
